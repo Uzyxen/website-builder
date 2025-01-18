b5b070b8f7199efc25b7b83ee2dd62164d766691
Uzyxen <michal912456@gmail.com>
2025-01-18 22:06:39 +0100

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-secondary text-5xl">Hello World!</h1>
+  </div>
+</template>
+
+<style>
+  body {
+    font-family: 'Roboto Condensed', serif;
+  }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      'primary': '#F6F7FF',
+      'secondary': '0A0D28'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
